3f1bbdb0bef7621a1a621d517c170ba24ecfab9c
Sergio Perez <sperez4mba@gmail.com.com>
2023-07-02 06:41:51 -0500

    Fix lint

--- src/landbot_challenge/channels/apps.py
+++ src/landbot_challenge/channels/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class ChannelsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'channels'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "channels"
